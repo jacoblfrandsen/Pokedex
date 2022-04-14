980575a45a074fda82bbffca27d0d1dd58e22f71
Jacob <jaocb.l.frandsen@gmail.com>
2022-04-13 23:28:19 -0600

    poke

---
+++ front-end/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+<div id="app">
+  <div class="header">
+    <router-link to="/">
+      <div class="logo">
+        <img src="/PokemonLogo.jpg" style="width:15em; height:7em;">
+      </div>
+    </router-link>
+    <div class="title">
+      <h1>Welcome to your own personal Pokedex</h1>
+    </div>
+    <div>
+      
+    </div>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <div class="footer">
+    <div class = "footer1">
+      <router-link to="/guess" class = footer1>Guess Pokemon</router-link>
+      <router-link to="/admin" class = footer1>Add/Delete Pokemon</router-link>
+      
+    </div>
+    
+    <p></p>
+    <a href="https://github.com/BYUCS260/museum-jacoblfrandsen">Github Repository</a> 
+  </div>
+</div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: '', Cambria;
+  font-size: 16px;
+  background: #fff;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header */
+.header {
+  display: flex;
+
+  flex-direction: column;
+  padding: 10px 100px 0px 100px;
+  background-color: #ffffff;
+  color: #1C454F;
+  content: center;
+  align-items: center;
+}
+
+.title {
+  margin-top: 40px;
+}
+
+.title h1 {
+  font-size: 30px;
+}
+
+
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+
+/* Footer */
+.footer {
+  display: flex;
+  flex-direction: column;
+  height: 100px;
+  padding: 50px 100px 0px 50px;
+  background: #5b5bd3;
+  font-size: 12px;
+  align-items: center;
+}
+
+.footer a {
+  color: #000;
+}
+
+.footer1{
+  font-size: 24px;
+  
+}
+
+h1 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 14px;
+}
+
+
+</style>
---
+++ front-end/src/views/Guess.vue
@@ -0,0 +1,115 @@
+<template>
+<div class="home">
+  <section class="image-gallery">
+    <div class="image">
+      
+      <img :src="item.path" />
+        
+    </div>
+    <form @submit.prevent="submit">
+        <input type="text" v-model="name"> 
+         <button type="submit">
+            Guess Name
+        </button>
+    </form>
+    <p style="white-space: pre-line;">{{ name }}</p>
+  </section>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+export default {
+  name: 'Home',
+  data() {
+    return {
+     item: null,
+     title: "",
+    name: ""
+
+    }
+  },
+  created() {
+    this.getItem();
+    this.checkName();
+  },
+  methods: {
+    async getItem() {
+      try {
+        let response = await axios.get("/api/items");
+        this.item = response.data.at(0);
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async checkName() {
+      try {
+        if (this.item.title == this.title)
+            return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    submit(){
+        axios.post('/your-url', {name: this.name})
+        try {
+            // if (this.item.title == this.title)
+            //     return true;
+        } catch (error) {
+            console.log(error);
+        }
+      },
+  },
+}
+</script>
+
+
+<style scoped>
+.image h2 {
+  font-style: italic;
+}
+
+/* Masonry */
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+.image-gallery {
+  column-gap: 1.5em;
+}
+
+.image {
+  margin: 0 0 1.5em;
+  display: inline-block;
+  width: 100%;
+}
+
+.image img {
+  width: 100%;
+}
+
+/* Masonry on large screens */
+@media only screen and (min-width: 1024px) {
+  .image-gallery {
+    column-count: 4;
+  }
+}
+
+/* Masonry on medium-sized screens */
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .image-gallery {
+    column-count: 3;
+  }
+}
+
+/* Masonry on small screens */
+@media only screen and (max-width: 767px) and (min-width: 540px) {
+  .image-gallery {
+    column-count: 2;
+  }
+}
+</style>
